bbcc405779ea682782e26800d1fd60b600b142c5
bryan-pinaffo <bryan.pinaffo.amp@gmail.com>
2024-04-26 10:03:29 -0300

    sla qual commit

--- src/JogoDeTexto/Dominio/Inimigo.java
+++ src/JogoDeTexto/Dominio/Inimigo.java
@@ -1,6 +1,6 @@
 package JogoDeTexto.Dominio;
 
-public class Inimigo extends Ser {
+public class Inimigo extends Pessoa {
 
 
     public Inimigo(String nome, int dano, int vida, int inteligencia, int forca) {
