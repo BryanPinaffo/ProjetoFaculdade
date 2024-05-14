23c34d5529e23793c81871dfcf302f80bba1fd81
Joao <57973684+WhaleJucs@users.noreply.github.com>
2024-05-14 17:05:22 -0300

    Att Menu

--- src/JogoDeTexto/Dominio/GameMenu.java
+++ src/JogoDeTexto/Dominio/GameMenu.java
@@ -1,9 +1,34 @@
 package JogoDeTexto.Dominio;
+import javax.swing.JOptionPane;
 
 public class GameMenu 
 {
     public static void showMenu(Player player)
     {
+        String[] options = {"Novo Jogo","Jogar","Salvar","Sair"};
 
+        switch () 
+        {
+            case "Novo Jogo":
+                
+            break;
+
+            case "Jogar":
+                
+            break;
+
+            case "Salvar":
+                
+            break;
+
+            case "Sair":
+                
+            break;
+
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inválida. Tente novamente.", "Erro", JOptionPane.ERROR_MESSAGE);
+                showMenu(player);
+            break;
+        }
     }
 }
