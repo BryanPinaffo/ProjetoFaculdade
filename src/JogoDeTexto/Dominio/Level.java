8b0f031f1d14cf7937399b92571537346da63fb4
Joao <57973684+WhaleJucs@users.noreply.github.com>
2024-05-14 16:24:05 -0300

    Levels novos comentados

--- src/JogoDeTexto/Dominio/Level.java
+++ src/JogoDeTexto/Dominio/Level.java
@@ -26,4 +26,76 @@ public class Level
         BattleSimulator battleSimulator = new BattleSimulator(player, enemy, textUtil);
         battleSimulator.startBattle();
     }
+
+    /*
+
+    public void level02(Player player) 
+    {
+        
+        // Exibir informações do jogador
+        String playerInfo = textUtil.playerProfile(player);
+        JOptionPane.showMessageDialog(null, playerInfo, "Ficha do Jogador", JOptionPane.INFORMATION_MESSAGE);
+
+        // Criar um inimigo para a fase 01
+        Enemy enemy = new Enemy("Esqueleto", 8, 50);
+        String enemyInfo = textUtil.enemyEncounter(enemy);
+        JOptionPane.showMessageDialog(null, enemyInfo, "Inimigo Encontrado!", JOptionPane.WARNING_MESSAGE);
+
+        // Iniciar batalha
+        BattleSimulator battleSimulator = new BattleSimulator(player, enemy, textUtil);
+        battleSimulator.startBattle();
+    }
+
+    public void level03(Player player) 
+    {
+        
+        // Exibir informações do jogador
+        String playerInfo = textUtil.playerProfile(player);
+        JOptionPane.showMessageDialog(null, playerInfo, "Ficha do Jogador", JOptionPane.INFORMATION_MESSAGE);
+
+        // Criar um inimigo para a fase 01
+        Enemy enemy = new Enemy("Esqueleto", 8, 50);
+        String enemyInfo = textUtil.enemyEncounter(enemy);
+        JOptionPane.showMessageDialog(null, enemyInfo, "Inimigo Encontrado!", JOptionPane.WARNING_MESSAGE);
+
+        // Iniciar batalha
+        BattleSimulator battleSimulator = new BattleSimulator(player, enemy, textUtil);
+        battleSimulator.startBattle();
+    }
+
+    public void level04(Player player) 
+    {
+        
+        // Exibir informações do jogador
+        String playerInfo = textUtil.playerProfile(player);
+        JOptionPane.showMessageDialog(null, playerInfo, "Ficha do Jogador", JOptionPane.INFORMATION_MESSAGE);
+
+        // Criar um inimigo para a fase 01
+        Enemy enemy = new Enemy("Esqueleto", 8, 50);
+        String enemyInfo = textUtil.enemyEncounter(enemy);
+        JOptionPane.showMessageDialog(null, enemyInfo, "Inimigo Encontrado!", JOptionPane.WARNING_MESSAGE);
+
+        // Iniciar batalha
+        BattleSimulator battleSimulator = new BattleSimulator(player, enemy, textUtil);
+        battleSimulator.startBattle();
+    }
+
+    public void levelFinal(Player player) 
+    {
+        
+        // Exibir informações do jogador
+        String playerInfo = textUtil.playerProfile(player);
+        JOptionPane.showMessageDialog(null, playerInfo, "Ficha do Jogador", JOptionPane.INFORMATION_MESSAGE);
+
+        // Criar um inimigo para a fase 01
+        Enemy enemy = new Enemy("Esqueleto", 8, 50);
+        String enemyInfo = textUtil.enemyEncounter(enemy);
+        JOptionPane.showMessageDialog(null, enemyInfo, "Inimigo Encontrado!", JOptionPane.WARNING_MESSAGE);
+
+        // Iniciar batalha
+        BattleSimulator battleSimulator = new BattleSimulator(player, enemy, textUtil);
+        battleSimulator.startBattle();
+    }
+
+    */
 }
